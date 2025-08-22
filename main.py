3e97e04d87e82226c27910a5b018aa696437de73
CharlesD <185566712+CharlesDdev@users.noreply.github.com>
2025-08-22 11:38:20 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from stats import count_words
+
+def get_book_text(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def main():
+    text = get_book_text('books/frankenstein.txt')
+    num_words = count_words(text)
+    
+    print(f"{num_words} words found in the document")
+    
+
+    
+if __name__ == '__main__':
+    main()
+    
